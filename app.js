5e33d2c22d108dc5048df0353ed9fab270d8ae92
muthylasreedharroyal <144820808+muthylasreedharroyal@users.noreply.github.com>
2023-10-23 12:34:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+let output = document.getElementById('input')
+function show(num) {
+    output.value = output.value + num
+}
+function calculate() {
+    try {
+        output.value = eval(output.value)
+    }
+    catch {
+        output.value = "Error"
+     }
+}
+function clr() {
+    output.value = ""
+}
+function remove(){
+    output.value = output.value.slice(0,-1)
+}
